59e0a9bb14c6f6c0be1f460b632f3111f6863c3c
carson135 <carson135@gmail.com>
2025-03-16 20:23:31 +0800

    d

--- ll/llp/settings.py
+++ ll/llp/settings.py
@@ -141,11 +141,15 @@ INTERNAL_IPS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),  #BASE_DIR / "staticfiles",
+    os.path.join(BASE_DIR, 'static'),  #BASE_DIR / "static",
 ]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+print(f"STATIC_URL: {STATIC_URL}")
+print(f"STATIC_ROOT: {STATIC_ROOT}")
+print(f"STATICFILES_DIRS: {STATICFILES_DIRS}")
