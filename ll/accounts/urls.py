881dc4a8c28fce365a616249aa0b06e9b78a0888
carson135 <carson135@gmail.com>
2025-03-22 19:29:38 +0800

    a

--- ll/accounts/urls.py
+++ ll/accounts/urls.py
@@ -9,9 +9,12 @@ app_name = 'accounts'
 
 urlpatterns = [
     # Login views
-    path('login/', views.phone_login, name='login'),        
+    path('login/', views.phone_login, name='login'),
+    path('sms-login/', views.sms_login, name='sms_login'),    
+    
     # Registration view
-    path('register/', views.register, name='register'),        
+    path('register/', views.register, name='register'),     
+    
     # Logout view
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('send-verification-code/', views.send_verification_code, name='send_verification_code'),
